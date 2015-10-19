269a9fb170c56eaa52b9e83558289a52558ace9f
Brennon Cupp <cuppb@student.swosu,edu>
2015-10-19 09:52:17 -0500

    started zodiac

--- src/ZodiacProject.java
+++ src/ZodiacProject.java
@@ -6,6 +6,13 @@ public class ZodiacProject {
 		System.out.print("Enter a Year ");
 		int year = input.nextInt();
 		
+		System.out.print("Enter a Month ");
+		int month = input.nextInt();
+		
+		System.out.print("Enter a Day ");
+		int day = input.nextInt();
+		
+		
 		switch (year % 12){
 			case 0: System.out.print("Monkey"); break;
 			case 1: System.out.print("Rooster"); break;
@@ -19,9 +26,22 @@ public class ZodiacProject {
 			case 9: System.out.print("Snake"); break;
 			case 10: System.out.print("Horse"); break;
 			case 11: System.out.print("Sheep"); break;
-		
 		}
-
+		switch (month){
+		case 1: System.out.print("Rooster"); break;
+		case 2: if (day < 20){ System.out.print("Aquarius"); break;}
+				else 		{System.out.print("Piseces");}
+		case 3: System.out.print("Pig"); break;
+		case 4: System.out.print("Rat"); break;
+		case 5: System.out.print("Ox"); break;
+		case 6: System.out.print("Tiger"); break;
+		case 7: System.out.print("Rabbit"); break;
+		case 8: System.out.print("Dragon"); break;
+		case 9: System.out.print("Snake"); break;
+		case 10: System.out.print("Horse"); break;
+		case 11: System.out.print("Sheep"); break;
+		case 12: System.out.print("");
 	}
 
+}
 }
